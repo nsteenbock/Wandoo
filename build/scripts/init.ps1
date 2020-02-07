cbbbfcb631ed8508e14a312125be43ad4e654626
Nils Steenbock <nils.steenbock@gmail.com>
2020-02-07 22:15:34 +0100

    Adding nuget feed for builds.

--- build/scripts/init.ps1
+++ build/scripts/init.ps1
@@ -1,5 +1,7 @@
 ﻿. $PSScriptRoot\config.ps1
 
+nuget sources add -Name MyAccountFeed -Source %NUGET_ACCOUNT_FEED% -UserName %NUGET_USER% -Password %NUGET_PWD%
+
 nuget install OpenCover -Version $opencoverversion -OutputDirectory $PsScriptRoot\..\tools
 nuget install coveralls.net -Version $coverallsversion -OutputDirectory $PsScriptRoot\..\tools
 nuget install xunit.runner.console -Version $xunitversion -OutputDirectory $PsScriptRoot\..\tools
